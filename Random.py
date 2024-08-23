0c9f88da8bd6034354bce75acd5a6d722174db0e
ananduajikumar <159014071+ananduajikumar@users.noreply.github.com>
2024-08-23 02:25:37 -0700

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,18 @@
+import random
+
+# Number of random integers to print
+num_integers = 5
+
+for _ in range(num_integers):
+    # Generate a random integer between 1 and 100 (inclusive)
+    random_integer = random.randint(1, 100)
+    print(random_integer)
+
+
+output
+
+71
+32
+51
+57
+40
